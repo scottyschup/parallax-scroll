cb0f1b976fe3aac3e0136b9d315448ba9f22e7cb
scottyschup <ssschupbach@gmail.com>
2015-07-07 02:42:54 -0700

    Make it go

--- parallaxScroll.js
+++ parallaxScroll.js
@@ -1,44 +1,82 @@
-// Let's make it so the divs don't need to be created by the user
-// Create the divs, add the css/image, start the scroll function
+// Options object must contain: containerId, backgroundImgUrl
+// it may also contain an optional fadeElementId and scrollSpeed (1 to 5)
 
 (function () {
-  if ($.fn.parallaxScroll) {
+  "use strict";
+  if (window.jQPS !== undefined) {
     return;
   } else {
-    var jQPS = $.fn.parallaxScroll = function () {
-      this.$container = $("#" + containerId);
-      this.$background = $("#" + containerId + " background")[0];
+    // how is $.fn this different from $.prototype?
+    window.jQPS = $.fn.ParallaxScroll = function (opts) {
+      this.$container = $("#" + opts.containerId);
+      this.$contentDiv = $("<div>")
+        .addClass("content")
+        .html(this.$container.html());
 
-      var body = window.document.body;
-      var html = window.document.documentElement;
+      this.$backgroundDiv = $("<div>")
+        .addClass("background")
+        .css({
+          "background": "url(" + opts.backgroundImgUrl + ")",
+          "background-size": "cover",
+          "-webkit-background-size": "cover",
+          "background-position": "center",
+          "position": "fixed",
+          "width": "100%",
+          "z-index": "-1"
+        });
 
-      this.height = Math.max(
-      body.scrollHeight,
-      body.offsetHeight,
-      html.clientHeight,
-      html.scrollHeight,
-      html.offsetHeight
-    );
+      this.$container
+        .html(this.$backgroundDiv)
+        .append(this.$contentDiv);
 
-    this.setBackgroundHeight();
-    this.scroll();
+      // var body = window.document.body;
+      // var html = window.document.documentElement;
+
+      // this.height = Math.max(
+      //   body.scrollHeight,
+      //   body.offsetHeight,
+      //   html.clientHeight,
+      //   html.scrollHeight,
+      //   html.offsetHeight
+      // );
+
+      this.height = $(document).height();
+      this.viewport = $(window).height();
+
+      if (opts.fadeElementId !== void 0) {
+        this.$fadeElement = $("#" + opts.fadeElementId)
+          .css({
+            // "position": "fixed",
+          })
+      } else {
+        this.$fadeElement = null;
+      }
+
+      if (opts.scrollSpeed !== void 0) {
+        this.scrollSpeed = opts.scrollSpeed;
+      } else {
+        this.scrollSpeed = 2;
+      }
+
+      this.setBackgroundHeight();
+      this.scroll();
     };
 
     jQPS.prototype.setBackgroundHeight = function () {
-      if ($(".background").length < 1) {
-        // create background div
-        window.document.create("div.background") // ? or whatever the fuck the syntax should be
-      }
-
-      $(".background").css("height", this.height + "px");
-  };
+      this.$backgroundDiv.css("height", this.height + "px");
+    };
 
     jQPS.prototype.scroll = function () {
+      var that = this;
       $(window).scroll(function () {
-      var st = $(this).scrollTop();
-      var yDelta = -st / 5;
-      $(".background").css("transform", "translateY(" + yDelta + "px)");
-    });
-    }
-  };
+        var st = $(this).scrollTop();
+        var yDelta = -st / that.scrollSpeed;
+        that.$backgroundDiv.css("transform", "translateY(" + yDelta + "px)");
+        if (that.$fadeElement) {
+          var perc = st / that.viewport * 1.75
+          that.$fadeElement.css("opacity", "calc(1 - " + perc + ")");
+        }
+      });
+    };
+  }
 })();
